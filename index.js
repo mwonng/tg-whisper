30f870afc60ec6cd5f7144846e4d36e01fa555f8
Mihcael <michael@wonng.com>
2020-02-13 15:28:09 +1100

    disable functions

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const welcome = require("./src/welcome/welcomeMsg");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 // bot.start(ctx => ctx.reply("Welcome"));
 
-// bot.help(ctx => ctx.reply("Send me a sticker"));
+bot.help(ctx => ctx.reply("Send me a sticker"));
 
-// bot.command("wow", ctx => ctx.reply("you are wow"));
+bot.command("wow", ctx => ctx.reply("you are wow"));
 // bot.on("sticker", ctx => ctx.reply("👍"));
 
 // bot.on("new_chat_members", ctx => ctx.reply("Welcome msg here"));
 
-welcome(bot);
+// welcome(bot);
 // bot.hears("hi", ctx => ctx.reply("Hey there"));
 // who(bot);
 
