821c2eafea9afd7a342485ea693c2c975f634504
Michael Wonng <michael@wonng.com>
2020-03-17 01:16:15 +1100

    disable able some usless function

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const welcome = require("./src/welcome/welcomeMsg");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 // bot.start(ctx => ctx.reply("Welcome"));
 
-bot.help(ctx => ctx.reply("Send me a sticker"));
+// bot.help(ctx => ctx.reply("Send me a sticker"));
 
-bot.command("wow", ctx => ctx.reply("you are wow"));
-bot.on("sticker", ctx => ctx.reply("👍"));
+// bot.command("wow", ctx => ctx.reply("you are wow"));
+// bot.on("sticker", ctx => ctx.reply("👍"));
 
 // bot.on("new_chat_members", ctx => ctx.reply("Welcome msg here"));
 
 welcome(bot);
-bot.hears("hi", ctx => ctx.reply("Hey there"));
+// bot.hears("hi", ctx => ctx.reply("Hey there"));
 // who(bot);
 
 bot.launch();
